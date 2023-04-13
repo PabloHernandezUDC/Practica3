b8436a28204aa98c98ed280de006e6c678a277ee
PabloHernandezUDC <125274113+PabloHernandezUDC@users.noreply.github.com>
2023-04-13 11:25:37 +0200

    avances

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 # Pablo Hernandez Martinez, pablo.hernandez.martinez@udc.es - Marcelo Ferreiro Sánchez, marcelo.fsanchez@udc.es
-from array_ordered_positional_list import ArrayPositionalList as PositionalList
-from linked_ordered_positional_list import LinkedPositionalLIst as PositionalList
-
+import sys
+from array_ordered_positional_list import ArrayOrderedPositionalList as PositionalList
+from linked_ordered_positional_list import LinkedOrderedPositionalList as PositionalList
+from libro import Libro
 
 def parse_params(params):
     '''
     '''
-    a, a = params[0], params[1]
+    titulo, autor = params[0:2]
+    año, prestamos = int(params[2]), int(params[3])
     
-        
-    return None # Objeto de la clase libro
+    return Libro(titulo, autor, año, prestamos)
 
 def run(path):
     '''
     '''
     with open(path) as f:
         for elemento in f.readlines():
-            a = parse_params(elemento.split())
+            a = parse_params(elemento.split('; '))
 
-    return stats
+    return None
 
 if __name__ == "__main__":
-    result = run(sys.argv[1])
+    result = run('libros.txt')
